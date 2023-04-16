4ce32c6d5a17d9891ec63f2ef9f4c843481a1f62
daichi0918 <kayasugadaichi@icloud.com>
2023-04-16 22:14:54 +0900

    finish 2.3.11

--- src/chapter2/assertion/assertion.test.ts
+++ src/chapter2/assertion/assertion.test.ts
@@ -181,3 +181,29 @@ test('triggered by the scheduled pipeline', () => {
     })
   );
 });
+
+class User {
+  name: string;
+  password: string;
+  constructor({ name, password }: { name: string; password: string }) {
+    // passwordが6文字未満の場合Errorをthrowする
+    if (password.length < 6)
+      throw new Error('The password length must be at least 6 characters.');
+    this.name = name;
+    this.password = password;
+  }
+}
+
+test('creates a new user with a 6-character password', () => {
+  expect(new User({ name: 'hoge', password: '123456' })).toEqual({
+    name: 'hoge',
+    password: '123456',
+  });
+});
+test('throw Error when the length of password is less than 6', () => {
+  expect(() => new User({ name: 'hoge', password: '12345' })).toThrow(); // Errorがthrowされたかのチェック
+  expect(() => new User({ name: 'hoge', password: '12345' })).toThrow(Error); //型のチェック
+  expect(() => new User({ name: 'hoge', password: '12345' })).toThrow(
+    'The password length must be at least 6 characters.'
+  ); //エラーメッセージのチェック
+});
